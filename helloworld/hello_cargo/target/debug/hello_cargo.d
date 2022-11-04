35136ffe44424efb3452fc0ce9bedd73e60b7fc6
Ida Delphine <idadelm@gmail.com>
2022-11-04 22:34:11 +0100

    hello world program using cargo

---
+++ helloworld/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/idadel/Desktop/Personal/Rust/learning-rust/helloworld/hello_cargo/target/debug/hello_cargo: /home/idadel/Desktop/Personal/Rust/learning-rust/helloworld/hello_cargo/src/main.rs
---
+++ helloworld/hello_cargo/target/rls/debug/deps/hello_cargo-ab67ca3ec9ebebea.d
@@ -0,0 +1,5 @@
+/home/idadel/Desktop/Personal/Rust/learning-rust/helloworld/hello_cargo/target/rls/debug/deps/hello_cargo-ab67ca3ec9ebebea.rmeta: src/main.rs
+
+/home/idadel/Desktop/Personal/Rust/learning-rust/helloworld/hello_cargo/target/rls/debug/deps/hello_cargo-ab67ca3ec9ebebea.d: src/main.rs
+
+src/main.rs:
