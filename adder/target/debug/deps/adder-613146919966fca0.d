eef955c99886f95c96c6133e3b796b2bb69e179b
Ida Delphine <idadelm@gmail.com>
2022-11-20 11:09:36 +0100

    learned about tests

---
+++ adder/target/debug/deps/adder-613146919966fca0.d
@@ -0,0 +1,5 @@
+/home/idadel/Desktop/Personal/Rust/learning-rust/adder/target/debug/deps/adder-613146919966fca0.rmeta: src/lib.rs
+
+/home/idadel/Desktop/Personal/Rust/learning-rust/adder/target/debug/deps/adder-613146919966fca0.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-d6c039b044ca65cb.d
@@ -0,0 +1,5 @@
+/home/idadel/Desktop/Personal/Rust/learning-rust/adder/target/debug/deps/adder-d6c039b044ca65cb.rmeta: src/lib.rs
+
+/home/idadel/Desktop/Personal/Rust/learning-rust/adder/target/debug/deps/adder-d6c039b044ca65cb.d: src/lib.rs
+
+src/lib.rs:
